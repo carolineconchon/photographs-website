83070bc0beca01e9943576eeb47f63b1edd2e0cc
Caroline Conchon <conchon.caroline@gmail.com>
2024-02-13 15:48:09 +0100

    backround

--- mon-projet-react/src/App.js
+++ mon-projet-react/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './components/Header';
 import Landing from './components/Landing';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <Landing/>
-      <Footer/>
     </div>
   );
 }
