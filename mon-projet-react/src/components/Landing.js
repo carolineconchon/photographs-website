500674bfb63d374c40c45a0c2a0934cabf34ef23
Caroline Conchon <conchon.caroline@gmail.com>
2024-02-13 17:03:53 +0100

    page acceuil mobile

--- mon-projet-react/src/components/Landing.js
+++ mon-projet-react/src/components/Landing.js
@@ -1,9 +1,15 @@
 import "../styles/landing.css"
-
-function Landing () {
+import bee from "../images/bee.jpg"
+function Landing() {
     return (
         <div className="landing">
-            <h1>This is the landing page</h1>
+            <div className="title">
+                <p className="title1">Soledad</p>
+                <p className="title2"> Photographie</p>
+            </div>
+            <div class="polaroid">
+                <img src={bee} alt="bee photograph"/>
+            </div>
         </div>
     )
 }
